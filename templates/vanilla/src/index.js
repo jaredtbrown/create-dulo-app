12c750c83a797c3ed2cfe4c5c69220decf91a90e
Jared Brown <brown.jared25@gmail.com>
2022-04-18 15:14:20 -0400

    Using dulo for vanilla template

--- templates/vanilla/src/index.js
+++ templates/vanilla/src/index.js
@@ -1,7 +1,15 @@
-window.renderApp = (rootElementId, args) => {
-  const element = document.getElementById(rootElementId);
-  const { greetings } = args;
+import { createApp } from '@jaredtbrown/dulo'
+
+const app = (props) => {
+  const { greetings } = props;
   const h1 = document.createElement('h1');
   h1.appendChild(document.createTextNode(greetings));
-  element.appendChild(h1);
+
+  return h1;
 }
+
+createApp({
+  name: 'app',
+  rootElementId: 'app',
+  component: app,
+})
